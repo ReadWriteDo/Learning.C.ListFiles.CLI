509304a64bd8e507ebd9602753dae35f21886b09
ReadWriteDo <riley.w.davis@outlook.com>
2025-11-02 13:06:17 -0800

    Got it to output the full file list in a directory.  Next step: Sorting

--- main.c
+++ main.c
@@ -2,9 +2,12 @@
 #include "printDirHandle.h"
 
 int main(int argc, char *argv[]){
+//    char dir[256];
     char *dir = argv[1];
+//    printf ("%s\n", "Input directory to scan.");
+//    scanf ("%255s", dir);
     
-    printf("%s\n",dir);
+    printf("%s\n", dir);
     printDir (dir);
     
     return 0;
--- printDirHandle.h
+++ printDirHandle.h
@@ -4,11 +4,17 @@
 
 DIR *printDir(char *workDir){
     char *error = "Failed to open directory";
-    DIR *printingDir = opendir("./");
+    DIR *printingDir = opendir(workDir);
     struct dirent *dirThatHasBeenRead = readdir(printingDir);
-    char *file1 = dirThatHasBeenRead->d_name;
-    long int pos = telldir(printingDir);
-    printf("%lu\n%s\n%ld\n",dirThatHasBeenRead->d_ino, file1,pos);
+    while(dirThatHasBeenRead != NULL){
+        char *file1 = dirThatHasBeenRead->d_name;
+        long int pos = telldir(printingDir);
+//        printf("%lu\n%s\n%ld\n",dirThatHasBeenRead->d_ino, file1,pos);
+        printf("%s\n",file1);
+        dirThatHasBeenRead = readdir(printingDir);
+    }
     rewinddir(printingDir);
+    closedir(printingDir);
+    
     return 0;
 }
